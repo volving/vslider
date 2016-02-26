d63fc7a5d818b95c7ba281e3b3cb2b7abd30c5d9
volving <volving@icloud.com>
2016-02-26 21:41:57 +0800

    Add: control buttons(prev, next)

--- fe/js/jquery.vslider.js
+++ fe/js/jquery.vslider.js
@@ -12,7 +12,7 @@
         magnifier: false,
         autoPlay: true,
         slideAnimation: '',
-        pause: 1000,
+        pause: 3000,
         speed: 250,
         interval: 10, // in ms
         rate: 25 // rate = speed / interval
@@ -30,16 +30,19 @@
             inlineSettings = {},
             settings = {},
             slideArray,
-            slideInterval,
-            clickSemophore = true;
-        inlineSettings.slideWidth = +($v.attr('data-width')) || 0;
-        inlineSettings.slideHeight = +($v.attr('data-height')) || 0;
+            slideIntervalId,
+            slideRestartTimeoutId,
+            clickSemophore = true,
+            $prev,
+            $next;
+        inlineSettings.slideWidth = parseInt($v.attr('data-width')) || 0;
+        inlineSettings.slideHeight = parseInt($v.attr('data-height')) || 0;
         settings = $.extend(true, defaults, options, inlineSettings);
 
         if ($s && (slideArray = $s.children('.slide'))) { //calc basic settings
             settings.index = 1;
             settings.slideAmount = slideArray.length;
-            settings.sliderWidth = settings.slideWidth * (settings.slideAmount + 2);
+            settings.slidesWidth = settings.slideWidth * (settings.slideAmount + 2);
             settings.sliderHeight = settings.slideHeight;
             settings.slideSpan = settings.slideWidth / settings.rate;
         } else {
@@ -52,35 +55,20 @@
         };
         utils.setSlideSize = function() {
             $s.children('.slide').children('a').children('img').css({ width: settings.slideWidth + 'px', height: settings.slideHeight + 'px' });
-            v.style.width = settings.sliderWidth + 'px';
-            v.style.left = this.calcPosition() + 'px';
+            v.style.width = settings.slideWidth + 'px';
+            s.style.width = settings.slidesWidth + 'px';
+            s.style.left = this.calcPosition() + 'px';
         };
         utils.setPosition = function() {
-            // $v.attr('style', 'width:' + settings.sliderWidth + 'px; height:' + settings.sliderHeight + 'px; left:' + this.getPosition() + 'px');
             this.calcPosition();
-            // var currentPosition = parseInt(v.style.left);
-            // var slideSpan = (currentPosition < settings.position) ? settings.slideSpan : -(settings.slideSpan);
-            // console.log('currentPosition:', currentPosition, '\nslideSpan:', slideSpan, '\nsettings.position:', settings.position);
-            // var slide_animate = function() {
-            //     if (currentPosition === settings.position) {
-            //         clickSemophore = true;
-            //         return;
-            //     } else {
-            //         currentPosition += slideSpan;
-            //         v.style.left = currentPosition + 'px';
-            //         setTimeout(slide_animate, settings.interval);
-            //     }
-            // };
-            // slide_animate();
-            // console.log(settings.index + '/' + settings.slideAmount);
-            $v.animate({ left: settings.position }, settings.speed, 'swing', function() {
+            $s.animate({ left: settings.position + 'px' }, settings.speed, 'swing', function() {
                 if (settings.index === (settings.slideAmount + 1)) {
                     settings.index = 1;
-                    v.style.left = utils.calcPosition() + 'px';
+                } else if (settings.index === 0) {
+                    settings.index = settings.slideAmount;
                 }
+                s.style.left = utils.calcPosition() + 'px';
             });
-
-
         };
         utils.setThumbnail = function() {
             $i.children('li:nth-child(' + settings.index + ')').addClass('active').siblings().removeClass('active');
@@ -100,6 +88,13 @@
 
         };
         utils.initNavs = function() {
+            var $navs = $('<div class="navs"><a href="#"><span class="prev">&lt;</span></a><a href="#"><span class="next">&gt;</span></a></div>');
+            if (!$n || 0 === $n.length) {
+                $n = $navs;
+                $s.after($n);
+            }
+            $prev = $navs.find('.prev');
+            $next = $navs.find('.next');
 
         };
         utils.initThumbnails = function() {
@@ -110,38 +105,65 @@
             this.initNavs();
             if (settings.indicator) { this.initIndicators(); }
             if (settings.thumbnail) { this.initThumbnails(); }
-
         };
         utils.setCss = function() {
             this.setPosition();
             if (settings.indicator) { this.setIndicator(); }
             if (settings.thumbnail) { this.setThumbnail(); }
         };
-        // prev / next
-        utils.count = function() {
-            var span = arguments[0] || 1;
-            span = (span > 0) ? 1 : -1;
-            console.log(settings.index);
-            settings.index = (settings.index + span) % (settings.slideAmount + 2);
-        }
-        utils.slides = function() {
-            clickSemophore = false;
-            this.count();
-            this.setCss();
-        };
-        utils.jump = function() {
+        utils.countIndex = function(steps) {
+            steps = parseInt(steps);
+            steps = isFinite(steps) ? steps : 1;
+            settings.index = (settings.index + steps) % (settings.slideAmount + 2);
 
-        }
+        };
+        utils.slides = function() {
+            // clickSemophore ? clickSemophore = false : return;
+            if (clickSemophore) {
+                clickSemophore = false;
+            } else {
+                return;
+            }
+            this.countIndex();
+            this.setCss();
+            clickSemophore = true;
+        };
+        utils.swap = function(steps) {
+            if (clickSemophore) {
+                clickSemophore = false;
+            } else {
+                return;
+            }
+            utils.pause();
+            clearTimeout(slideRestartTimeoutId);// restart sliding
+            console.log('settings.index:', settings.index, steps);
+            this.countIndex(steps);
+            this.setCss();
+            slideRestartTimeoutId = setTimeout(utils.play, settings.interval);
+        };
+        utils.prev = function() {
+            utils.swap(-1);
+        };
+        utils.next = function() {
+            utils.swap(1);
+        };
         utils.play = function() {
-            slideInterval = setInterval(function() {
+            slideIntervalID = setInterval(function() {
                 utils.slides();
             }, settings.pause);
+            clickSemophore = true;
         };
         utils.pause = function() {
-            clearInterval(slideInterval);
+            clearInterval(slideIntervalID);
         };
         utils.bindEventHandler = function() {
             $s.on('mouseover', utils.pause).on('mouseleave', utils.play);
+            if ($prev) {
+                $prev.on('click', utils.prev);
+            }
+            if ($next) {
+                $next.on('click', utils.next);
+            }
         };
         // -----------------------------------------------End __of Setting up configures
 
